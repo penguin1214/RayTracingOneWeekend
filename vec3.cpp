72bbea3f1e3347a5c9d0f9c95f9427fdaf391248
penguin <penguin1214@users.noreply.github.com>
2017-07-29 11:29:25 +0800

    modify vec3

--- vec3.cpp
+++
@@ -1,6 +0,0 @@
-//
-// Created by Jinglei Yang on 7/28/2017.
-//
-
-#include "vec3.h"
-
--- vec3.h
+++ vec3.h
@@ -20,9 +20,15 @@ class vec3 {
 public:
     // constructor
     vec3(){}
+
     vec3(float e0, float e1, float e2) {
         e[0] = e0; e[1] = e1; e[2] = e2;
     }
+
+    vec3(float f) {
+        e[0] = f; e[1] = f; e[2] = f;
+    }
+
     inline float x() const { return e[0];}
     inline float y() const { return e[1];}
     inline float z() const { return e[2];}
@@ -40,14 +46,10 @@ public:
     inline vec3&operator /= (const float f);
 
 
-    inline float length() {
+    inline float length() const{
         return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
     }
 
-    inline vec3 uniform() {
-        float l = length();
-        return vec3(e[0]/l, e[1]/l, e[2]/l);
-    }
 };
 
 
@@ -130,6 +132,14 @@ inline vec3 cross(const vec3& v1, const vec3& v2) {
     return vec3(v1.e[1]*v2.e[2] - v1.e[2]*v2.e[1],
     -(v1.e[0]*v2.e[2] - v1.e[2]*v2.e[0]),
     v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]);
+}
+
+// unit vector
+// remove a member function uniform(), since when a const vec3 object call uniform(),
+// the compiler would throw an error, because if uniform() is a non-const function,
+// calling a non-const function DO NOT guarantee the const object's constness.
+inline vec3 unit(const vec3&v) {
+    return vec3(v.e[0]/v.length(), v.e[1]/v.length(), v.e[2]/v.length());
 }
 
 inline std::istream& operator>>(std::istream& is, vec3& v) {
