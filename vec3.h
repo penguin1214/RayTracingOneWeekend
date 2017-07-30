fd30b2b6fc703b90dfd403f66b00b156f1f900f7
penguin <penguin1214@users.noreply.github.com>
2017-07-30 17:00:17 +0800

    modify vec3

--- vec3.h
+++ vec3.h
@@ -32,6 +32,9 @@ public:
     inline float x() const { return e[0];}
     inline float y() const { return e[1];}
     inline float z() const { return e[2];}
+    inline float r() const { return e[0];}
+    inline float g() const { return e[1];}
+    inline float b() const { return e[2];}
 
     // member
     float e[3];
@@ -45,6 +48,8 @@ public:
     inline vec3&operator *= (const float f);
     inline vec3&operator /= (const float f);
 
+    inline const vec3&operator +() const { return *this;}
+    inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]);}
 
     inline float length() const{
         return sqrt(e[0]*e[0] + e[1]*e[1] + e[2]*e[2]);
@@ -95,8 +100,6 @@ inline vec3&vec3::operator/=(const float f) {
     return *this;
 }
 
-// non-member function
-
 inline vec3 operator+(const vec3& v1, const vec3& v2) {
     return vec3(v1.e[0]+v2.e[0], v1.e[1]+v2.e[1], v1.e[2]+v2.e[2]);
 }
@@ -104,7 +107,6 @@ inline vec3 operator+(const vec3& v1, const vec3& v2) {
 inline vec3 operator-(const vec3& v1, const vec3& v2) {
     return vec3(v1.e[0]-v2.e[0], v1.e[1]-v2.e[1], v1.e[2]-v2.e[2]);
 }
-
 
 inline vec3 operator/(const vec3& v1, const vec3& v2) {
     return vec3(v1.e[0]/v2.e[0], v1.e[1]/v2.e[1], v1.e[2]/v2.e[2]);
